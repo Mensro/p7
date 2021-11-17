3f470b1bdb6bc7ff20938e7c17a46cbb8eb9ce78
alexandre romens <alexandreromens57@gmail.com>
2021-11-17 15:12:15 +0100

    init

---
+++ vue-app/src/components/navlink.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'homelink',
+    props: ['url', 'text'],
+    
+}
+</script>
+
+<template>
+    <a href="url">{{ text }}</a>
+</template>
+<style>
+a {
+    text-decoration: none;
+
+}
+
+</style>
