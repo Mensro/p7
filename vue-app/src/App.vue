7a5a116597e3779d676d072322e179b6597fb200
alexandre romens <alexandreromens57@gmail.com>
2021-11-17 15:13:08 +0100

    init

---
+++ vue-app/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import NavLink from './components/navlink.vue'
+
+export default {
+	name: 'App',
+	components: {
+		NavLink
+	}
+}
+</script>
+
+<template>
+	<div id="app">
+		<nav>
+      <navLink url="/" text="Acceuil" />
+      <navLink url="/about" text="A propos" />
+      <navLink url="/contact" text="Contact" />
+			
+		</nav>
+		
+	</div>
+</template>
