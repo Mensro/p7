56db293cc24b091502430b98073c985958c13a9e
alexandre romens <alexandreromens57@gmail.com>
2022-02-23 20:37:24 +0100

    postdone need image

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -35,6 +35,61 @@
             </div>
 
             <div class="bg-white border mt-2 shadow">
+              <div class="postsList">
+                <div class="postsItem" v-for="post in posts" :key="post.id">
+                  {{ post.description }}
+
+                  <div class="bg-white border mt-2 shadow">
+                    <div>
+                      <div
+                        class="
+                          d-flex
+                          flex-row
+                          justify-content-between
+                          align-items-center
+                          p-2
+                          border-bottom
+                        "
+                      >
+                        <div
+                          class="
+                            d-flex
+                            flex-row
+                            align-items-center
+                            feed-text
+                            px-2
+                          "
+                        >
+                          <img
+                            class="rounded-circle"
+                            src="https://pbs.twimg.com/media/FCJQu2QWYAMo5Vg.jpg"
+                            width="45"
+                          />
+                          <div class="d-flex flex-column flex-wrap ml-2">
+                            <span class="font-weight-bold"
+                              >traytonette image only</span
+                            ><span class="text-black-50 time"
+                              >40 minutes ago</span
+                            >
+                          </div>
+                        </div>
+                        <div class="feed-icon px-2">
+                          <i class="fa fa-ellipsis-v text-black-50"></i>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="feed-image p-2 px-3">
+                      {{ post.description }}
+                      {{ post.imageUrl }}
+                    </div>
+                    <div class="d-flex justify-content-end socials p-2 py-3">
+                      <i class="fa fa-thumbs-up"></i
+                      ><i class="fa fa-comments-o"></i
+                      ><i class="fa fa-share"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div>
                 <div
                   class="
@@ -186,7 +241,13 @@ export default {
     return {
       description: "",
       imageUrl: "",
-      posts: [],
+      posts: [
+        {
+          description: String,
+          imageUrl: String,
+          createdAt: String,
+        },
+      ],
     };
   },
   methods: {
@@ -207,7 +268,17 @@ export default {
       console.log("userpost", this.description);
     },
     async getAllPost() {
-      this.posts = [{ description: "test" }, { description: "test2" }];
+      const response = await fetch("http://localhost:8082/api/posts", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          authorization: "Bearer " + this.$store.state.token,
+        },
+      });
+      const data = await response.json();
+      console.log("data", data);
+
+      this.posts = data;
     },
   },
   mounted() {
